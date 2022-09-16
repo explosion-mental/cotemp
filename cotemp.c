ce71028864d0090e66e1f4ec63bc22d193f00ecc
explosion-mental <explosion0mental@gmail.com>
2022-09-15 23:16:14 -0500

    refactor macros a bit

--- cotemp.c
+++ cotemp.c
@@ -8,35 +8,36 @@
 #include <string.h>
 #include <math.h>
 
-#define TEMPERATURE_NORM    6500
-#define TEMPERATURE_ZERO    700
-#define GAMMA_MULT          65535.0
+/* macros */
+#define MAX(A, B)               ((A) > (B) ? (A) : (B))
+#ifdef DEBUG
+#define debug(...)              do { fprintf(stderr, "cotemp(debug): %s:\n", __func__); fprintf(stderr, "\t" __VA_ARGS__); } while (0)
+#else
+#define debug(...)
+#endif /* DEBUG */
+#define TEMPERATURE_NORM        6500
+#define TEMPERATURE_ZERO        700
+#define GAMMA_MULT              65535.0
+#define BRIGHTHESS_DIV          65470.988
+
 // Approximation of the `redshift` table from
 // https://github.com/jonls/redshift/blob/04760afe31bff5b26cf18fe51606e7bdeac15504/src/colorramp.c#L30-L273
 // without limits:
 // GAMMA = K0 + K1 * ln(T - T0)
 // Red range (T0 = TEMPERATURE_ZERO)
 // Green color
-#define GAMMA_K0GR          -1.47751309139817
-#define GAMMA_K1GR          0.28590164772055
+#define GAMMA_K0GR             -1.47751309139817
+#define GAMMA_K1GR              0.28590164772055
 // Blue color
-#define GAMMA_K0BR          -4.38321650114872
-#define GAMMA_K1BR          0.6212158769447
+#define GAMMA_K0BR             -4.38321650114872
+#define GAMMA_K1BR              0.6212158769447
 // Blue range  (T0 = TEMPERATURE_NORM - TEMPERATURE_ZERO)
 // Red color
-#define GAMMA_K0RB          1.75390204039018
-#define GAMMA_K1RB          -0.1150805671482
+#define GAMMA_K0RB              1.75390204039018
+#define GAMMA_K1RB             -0.1150805671482
 // Green color
-#define GAMMA_K0GB          1.49221604915144
-#define GAMMA_K1GB          -0.07513509588921
-#define BRIGHTHESS_DIV      65470.988
-
-#define MAX(A, B)               ((A) > (B) ? (A) : (B))
-#ifdef DEBUG
-#define debug(...)		do { fprintf(stderr, "cotemp(debug): %s:\n", __func__); fprintf(stderr, "\t" __VA_ARGS__); } while (0)
-#else
-#define debug(...)
-#endif /* DEBUG */
+#define GAMMA_K0GB              1.49221604915144
+#define GAMMA_K1GB             -0.07513509588921
 
 /* variables */
 static Display *dpy;
