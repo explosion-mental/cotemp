873b3a2f42b6275dbac3811d5020292bd3eb0e0e
explosion-mental <explosion0mental@gmail.com>
2022-09-16 12:15:58 -0500

    fix night profile, should be 18, not 06

--- config.h
+++ config.h
@@ -10,5 +10,5 @@
 static const Profile profiles[] = {
 	/* name          from             to      temperature  brightness */
 	{ "day",	"05:00",	"11:45",	6500,	1.0 },
-	{ "night",	"12:00",	"24:00",	3000,	0.7 },
+	{ "night",	"18:00",	"24:00",	3000,	0.7 },
 };
